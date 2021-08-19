6f6e81e7a4784981e15620a49115db664bee2eae
Raul Coelho <rpc3@cin.ufpe.br>
2021-08-19 18:04:53 -0300

    app DONE

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ f = open("/dev/char_device", "w")  # open
 f.write("testing")                 # write
 f.close()                          # release
 f = open("/dev/char_device", "r")  # open
-string = f.read(17)                # read
+string = f.read()                  # read
 f.close()                          # release
 print(string)                      # helloworldtesting
 
@@ -18,7 +18,7 @@ f = open("/dev/char_device", "w")  # open
 f.write("driver")                  # write
 f.close()                          # release
 f = open("/dev/char_device", "r")  # open
-string = f.read(20)                # read
+string = f.read()                  # read
 f.close()                          # release
 print(string)                      # verloworldtestingdri
 
@@ -26,6 +26,6 @@ f = open("/dev/char_device", "w")  # open
 f.write("ABCD")                    # write
 f.close()                          # release
 f = open("/dev/char_device", "r")  # open
-string = f.read(20)                # read
+string = f.read()                  # read
 f.close()                          # release
 print(string)                      # verABCDrldtestingdri
